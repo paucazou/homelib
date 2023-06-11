05273002832adc1831a2776eea1d786dc8793d63
Philippe Aucazou <paucazou@yahoo.fr>
2023-06-11 18:48:52 +0200

    Added a script to calculate the number of volumes in the db

--- volumes.py
+++ volumes.py
@@ -1,6 +1,18 @@
 """Calcule le nombre de volumes d'après les détails supplémentaires. Pas tout à fait au point"""
 
+import csv
+import hashlib
 import mainclass
+import re
+
+sums_file = "sums.csv"
+# this global variable can be modified in the functions. Be careful
+with open(sums_file, newline='') as f:
+    sums = { elt[0] : int(elt[1]) for elt in csv.reader(f) }
+
+with open("sums.csv") as f:
+    # loading sums of the details to do not repeat manual entries
+    details_sums = f.read()
 
 def to_int(number: int | str) -> int:
     if isinstance(number, int):
@@ -37,7 +49,11 @@ def to_int(number: int | str) -> int:
 def volumes_nb(txt: str) -> int:
     """Parses the txt to find the keywords 'tomes' or 'volumes'
     and return the number found before it"""
-    txt = txt.lower()
+    old_txt = txt
+    txt = re.sub("[\.]",
+                 "",
+                 txt.lower())
+    txt = re.sub("[\n]"," ",txt)
     if not ('volumes' in txt or 'tomes' in txt):
         return 1 # why 1? Because there is at least one volume!
     l = txt.split()
@@ -47,8 +63,26 @@ def volumes_nb(txt: str) -> int:
             try:
                 nb += to_int(l[i-1])
             except ValueError:
-                print(txt)
                 continue
+    if nb == 0:
+        # try to find the value in sums.csv
+        hashval = hashlib.md5(old_txt.encode())
+        nb = sums.get(hashval.hexdigest(),0)
+    if nb == 0:
+        # no volum has been found: let's ask the user
+        print(old_txt)
+        while nb == 0:
+            try:
+                nb = int(input("Please enter the correct number: "))
+            except ValueError:
+                nb = 0
+
+        print(hashval.hexdigest())
+        with open(sums_file, 'a', newline='') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([hashval.hexdigest(), nb])
+
+        sums[hashval.hexdigest()] = nb
     return nb
 
 def volumes_nb_in_db(db: str) -> int:
